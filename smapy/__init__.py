8c07fe48f7f148f2f0f79bd88d81ac35de3f2aec
Alexander Tolmach <quatsch@me.com>
2014-01-27 09:06:25 -0500

    project dir created

---
+++ smapy/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+"""smapy - init file"""
+
+__version__ = '0.3.1'
+__author__ = 'Alexander Tolmach <tolmach@me.com>'
+__license__ = 'MIT'
+
+import settings
