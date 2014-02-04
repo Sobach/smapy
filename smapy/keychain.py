d2c62e56363eccde80d69a687940e899e6973636
Alexander Tolmach <quatsch@me.com>
2014-02-04 03:37:39 -0500

    KeyChain bug fixes

--- smapy/keychain.py
+++ smapy/keychain.py
@@ -3,6 +3,7 @@
 
 #import smapy
 from smapy import CONNECTORS
+from smapy.http_utilities import vk_auth, fb_auth, ok_auth
 import pprint
 import datetime
 import pickle
@@ -18,36 +19,67 @@ class KeyChain(object):
 
     def assign(self, net, token, validate = True, check = False):
         self._networks = CONNECTORS.keys()
-        if net in self._networks and self.token_format_validator(net, token):
+        if validate and not self._token_format_validator(net, token):
+            logging.error(u'KEYCHAIN: Token wasn\'t validated by service.')
+            return
+        if net in self._networks:
+            if check and not self._checker(net, token):
+                logging.error(u'KEYCHAIN: Token wasn\'t validated by service.')
+                return
             self._keys[net] = token
             if net == 'yt':
                 self._keys['gp'] = token
             elif net == 'gp':
                 self._keys['yt'] = token
             self.autocomplete()
-        elif net in ('raw_vk', 'raw_fb', 'raw_ok') and self.token_format_validator(net, token):
+        elif net in ('raw_vk', 'raw_fb', 'raw_ok'):
+            if check:
+                if net == 'raw_vk' and not self._checker('vk', vk_auth(token)):
+                    logging.error(u'KEYCHAIN: Token wasn\'t validated by service.')
+                    return
+                if net == 'raw_fb' and not self._checker('fb', fb_auth(token)):
+                    logging.error(u'KEYCHAIN: Token wasn\'t validated by service.')
+                    return
+                if net == 'raw_ok' and not self._checker('ok', ok_auth(token)[0]):
+                    logging.error(u'KEYCHAIN: Token wasn\'t validated by service.')
+                    return
             self._keys[net] = token
             self.autocomplete()
         else:
-            logging.error(u'KEYCHAIN: Token wasn\'t assigned: unknown network identifier.'.format(net.upper()))
+            logging.error(u'KEYCHAIN: Token wasn\'t assigned: unknown network identifier.')
             return
 
-    def token_format_validator(self, net, token):
-        token_formats = {'ok':set(['app_key', 'app_secret', 'access_token']),
-                         'tw':set(['consumer_key','consumer_secret','access_token','access_secret']),
-                         'raw_ok':set(['app_id','app_key','app_secret','login','password']),
-            }
-        if net not in token_formats:
-            if isinstance(token, (str, unicode)):
-                return True
-            else:
-                logging.error(u'KEYCHAIN: Token for {} should be <string> or <unicode> type'.format(net.upper()))
-                return False
-        if isinstance(token, dict) and token_formats[net].difference(set(token.keys())):
-                return True
-        else:
-            logging.error(u'KEYCHAIN: Token for {} should be dict with these keys: {}'.format(net.upper(), ', '.join(token_formats[net])))
+    def check(self, net = None, token = None):
+        self._networks = CONNECTORS.keys()
+        if not net and token:
+            logging.error(u'KEYCHAIN: Specify network, your key belongs to.')
             return False
+
+        elif not net and not token:
+            ret_dict = {}
+            self.autocomplete()
+            for network in list(set(CONNECTORS.keys()).intersection(set(self._keys.keys()))):
+                ret_dict[network] = self._checker(network, self._keys[network])
+            return ret_dict
+
+        elif net and not token:
+            if net not in self._keys or net not in self._networks:
+                logging.error(u'KEYCHAIN: There is no key for \'{}\' in this KeyChain object.'.format(net))
+                return False
+            if net == 'fb' and 'raw_fb' in self._keys:
+                self._keys['fb'] = fb_auth(self._keys['raw_fb'])
+
+            elif net == 'vk' and 'raw_vk' in self._keys:
+                self._keys['vk'] = vk_auth(self._keys['raw_vk'])
+
+            elif net == 'ok' and 'raw_ok' in self._keys:
+                self._keys['ok'], self._keys['raw_ok'] = ok_auth(self._keys['raw_ok'])
+            return self._checker(net, self._keys[net])
+        else:
+            if net not in self._networks:
+                logging.error(u'KEYCHAIN: There is no connector for \'{}\'.'.format(net))
+                return False
+            return self._checker(net, token)
 
     def get(self, net = None):
         if not net:
@@ -62,17 +94,20 @@ class KeyChain(object):
 
     def autocomplete(self):
         if 'raw_vk' in self._keys.keys():
-            self._keys['vk'] = smapy.http_utilities.vk_auth(self._keys['raw_vk'])
+            self._keys['vk'] = vk_auth(self._keys['raw_vk'])
 
         if 'raw_fb' in self._keys.keys():
-            self._keys['fb'] = smapy.http_utilities.fb_auth(self._keys['raw_fb'])
+            self._keys['fb'] = fb_auth(self._keys['raw_fb'])
+
         if 'raw_ok' in self._keys.keys():
-            self._keys['ok'], self._keys['raw_ok'] = smapy.http_utilities.ok_auth(self._keys['raw_ok'])
+            self._keys['ok'], self._keys['raw_ok'] = ok_auth(self._keys['raw_ok'])
 
         if 'lj' not in self._keys.keys():
             self._keys['lj'] = ''
+
         if 'yt' in self._keys.keys() and 'gp' not in self._keys.keys():
             self._keys['gp'] = self._keys['yt']
+
         if 'gp' in self._keys.keys() and 'yt' not in self._keys.keys():
             self._keys['yt'] = self._keys['gp']
 
@@ -120,46 +155,31 @@ class KeyChain(object):
         except IndexError:
             logging.error(u'KEYCHAIN: No dumps to load.')
 
-    def check(self, net = None, token = None):
-        self._networks = CONNECTORS.keys()
-        if not net and token:
-            logging.error(u'KEYCHAIN: Specify network, your key belongs to.')
-            return False
-
-        elif not net and not token:
-            ret_dict = {}
-            self.autocomplete()
-            for network in list(set(CONNECTORS.keys()).intersection(set(self._keys.keys()))):
-                ret_dict[network] = self._checker(network, self._keys[network])
-            return ret_dict
-
-        elif net and not token:
-            if net not in self._keys or net not in self._networks:
-                logging.error(u'KEYCHAIN: There is no key for \'{}\' in this KeyChain object.'.format(net))
-                return False
-            if net == 'fb' and 'raw_fb' in self._keys:
-                self._keys['fb'] = fb_auth(self._keys['raw_fb']['app_id'],
-                               self._keys['raw_fb']['app_secret'],
-                               self._keys['raw_fb']['app_url'],
-                               self._keys['raw_fb']['login'],
-                               self._keys['raw_fb']['password'])
-            elif net == 'vk' and 'raw_vk' in self._keys:
-                self._keys['vk'] = vk_auth(self._keys['raw_vk']['app_id'],
-                               self._keys['raw_vk']['app_secret'],
-                               self._keys['raw_vk']['login'],
-                               self._keys['raw_vk']['password'])
-            return self._checker(net, self._keys[net])
-        else:
-            if net not in self._networks:
-                logging.error(u'KEYCHAIN: There is no connector for \'{}\'.'.format(net))
-                return False
-            return self._checker(net, token)
-
     def _checker(self, network, key):
         conn = CONNECTORS[network]['connector']
+        if not self._token_format_validator(network, key):
+            logging.info(u'KEYCHAIN: Token for {} is not valid.'.format(CONNECTORS[network]['networkname']))
+            return False
         result = conn(token = key).check_token()
         if result:
             logging.info(u'KEYCHAIN: Token for {} is valid.'.format(CONNECTORS[network]['networkname']))
         else:
             logging.info(u'KEYCHAIN: Token for {} is not valid.'.format(CONNECTORS[network]['networkname']))
         return result
+
+    def _token_format_validator(self, net, token):
+        token_formats = {'ok':set(['app_key', 'app_secret', 'access_token']),
+                         'tw':set(['consumer_key','consumer_secret','access_token','access_secret']),
+                         'raw_ok':set(['app_id','app_key','app_secret','login','password']),
+            }
+        if net not in token_formats:
+            if isinstance(token, (str, unicode)):
+                return True
+            else:
+                logging.error(u'KEYCHAIN: Token for {} should be <string> or <unicode> type'.format(net.upper()))
+                return False
+        if isinstance(token, dict) and not token_formats[net].difference(set(token.keys())):
+            return True
+        else:
+            logging.error(u'KEYCHAIN: Token for {} should be dict with these keys: {}'.format(net.upper(), ', '.join(token_formats[net])))
+            return False
